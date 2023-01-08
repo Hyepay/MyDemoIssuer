e7fd1f882250245df1fa9deb7aa5dd09e1e8f464
Otp <opentrustpay@gmail.com>
2023-01-08 01:35:27 +0100

    Support Serilog

--- MyDemoIssuer/Program.cs
+++ MyDemoIssuer/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Create Serilog logger 
+
+Serilog.Core.Logger logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .Enrich.FromLogContext()
+  .CreateLogger();
+
 
 // Add services to the container.
 
@@ -6,6 +18,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// add serliog to builder 
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 var app = builder.Build();
 
